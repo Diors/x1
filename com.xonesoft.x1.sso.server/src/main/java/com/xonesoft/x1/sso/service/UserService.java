227fa21de2eb172ce24a485f11a622cebe0a1fc5
Qian Huanping <qian.huanping@gmail.com>
2020-12-24 14:01:56 +0800

    clean

--- com.xonesoft.x1.sso.server/src/main/java/com/xonesoft/x1/sso/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.xonesoft.x1.sso.service;
-
-import com.xonesoft.x1.sso.core.model.UserInfo;
-import com.xonesoft.x1.sso.core.result.ReturnT;
-
-public interface UserService {
-	 public ReturnT<UserInfo> findUser(String username, String password);
-}
